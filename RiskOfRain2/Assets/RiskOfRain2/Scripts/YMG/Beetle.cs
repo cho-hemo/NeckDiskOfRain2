04c55342314be782c44c8b9222a30350de34fdfd
ymg <freedman0829@naver.com>
2023-03-15 17:39:14 +0900

    Dev 0315 방향전환, 박치기

--- RiskOfRain2/Assets/RiskOfRain2/Scripts/YMG/Beetle.cs
+++ RiskOfRain2/Assets/RiskOfRain2/Scripts/YMG/Beetle.cs
@@ -7,9 +7,13 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class Beetle : Monster
 {
-    public LayerMask targetLayer; // 추적 대상 레이어
-    [SerializeField] Transform target;
+    public float lookRange = 20f; // 시야 거리
+    public float attackRange = 3f; // 공격 거리
+    Transform target;
     NavMeshAgent navMeshAgent;
+
+    public LayerMask targetLayer; // 추적 대상 레이어
+    public LayerMask movementMask;
 
     public Animator beetleAnimator;
 
@@ -18,47 +22,115 @@ public class Beetle : Monster
     private float timeBetAttack = 0.5f; // 공격 간격
     private float lastAttackTime;  // 마지막 공격 시점
 
-    public float lookRange = default; // 시야 거리
-
     public string spawnAnime = ""; // 생성 애니메이션
     public string dieAnime = ""; // 사망 애니메이션
     private string moveAnime = "isMove";
-    public string attckAnime = "";
+    private string attackAnime = "isAttack";
     public string idleAnime = "";
 
     string nowMode = "";
 
+    Camera cam;
+
     void Start()
     {
-        nowMode = idleAnime;
+        cam = Camera.main;
+        target = PlayerManager.instance.player.transform;
         navMeshAgent = GetComponent<NavMeshAgent>();
-        this.GetComponent<Animator>().Play(spawnAnime);
+
+        //nowMode = idleAnime;
+        //this.GetComponent<Animator>().Play(spawnAnime);
         //navMeshAgent.updatePosition = false;
         //navMeshAgent.updateRotation = false;
-
-        beetleAnimator.SetBool(moveAnime, true);
+        //beetleAnimator.SetBool(moveAnime, true);
     }
 
     void Update()
     {
-        // navMeshAgent.SetDestination(target.position);
-        //this.GetComponent<Animator>().Play();
-        //if (beetleAnimator.GetBool(moveAnime) == false)
-        //{
-        //    beetleAnimator.SetBool(moveAnime, true);
-        //}
+        //FaceTarget();
 
-        Debug.Log(beetleAnimator.deltaPosition);
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRange)
+        {
+
+            navMeshAgent.SetDestination(target.position);
+
+            if (distance <= navMeshAgent.stoppingDistance)
+            {
+                // 목표 공격
+
+                // 목표 보기
+                FaceTarget();
+            }
+
+        }
+        //Debug.Log($"{distance}");
+        if (distance <= attackRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+
+                //Debug.Log($"박치기 / 비틀의 거리 {distance} 멈추는 지점 == {navMeshAgent.stoppingDistance}");
+            if (distance <= navMeshAgent.stoppingDistance + attackRange)
+            {
+                Debug.Log("비틀이 공격함");
+                navMeshAgent.SetDestination(transform.position);
+                CloseCombat();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, movementMask)) 
+            {
+                Debug.Log("We hit" + hit.collider.name + "" + hit.point);
+                MoveToPoint(hit.point);
+            }
+        }
+
+        //this.GetComponent<Animator>().Play();
+        //Debug.Log(beetleAnimator.deltaPosition);
         //Debug.Log(beetleAnimator.hasRootMotion);
         //transform.position = beetleAnimator.rootPosition;
     }
 
+    public void MoveToPoint(Vector3 point) 
+    {
+        navMeshAgent.SetDestination(point);
+        if (beetleAnimator.GetBool(moveAnime) == false)
+        {
+            beetleAnimator.SetBool(moveAnime, true);
+        }
+    }
 
+    void FaceTarget() 
+    {
+        // direction to the target
+        Vector3 direction = (target.position - transform.position).normalized;
+        // rotation where we point to that target
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        // update our own rotation to point in this direction
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    public override void CloseCombat()
+    {
+        base.CloseCombat();
+
+        if (beetleAnimator.GetBool(attackAnime) == false)
+        {
+            beetleAnimator.SetBool(attackAnime, true);
+        }
+    }
 
     void OnDrawGizmosSelected() 
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 
     public override void Die()
