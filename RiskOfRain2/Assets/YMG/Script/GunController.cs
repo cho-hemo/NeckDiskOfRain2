2689ad018fe9e4b78addbf602f267ff0e5825207
ymg <freedman0829@naver.com>
2023-03-17 14:53:48 +0900

    Dev 0317 에셋 교통정리

--- RiskOfRain2/Assets/YMG/Script/GunController.cs
+++ RiskOfRain2/Assets/YMG/Script/GunController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
-{
+{ // 무기를 장비하는 등의 관리
+
     public Transform weaponHold;
     public Gun startingGun;
-    Gun equippedGun;
+    Gun equippedGun; // 장착중인 총 
 
     void Start() 
     {
@@ -16,7 +17,7 @@ public class GunController : MonoBehaviour
         }
     }
 
-    public void EquipGun(Gun gunToEquip) 
+    public void EquipGun(Gun gunToEquip) // 장착할 총 
     {
         if (equippedGun != null) 
         {
