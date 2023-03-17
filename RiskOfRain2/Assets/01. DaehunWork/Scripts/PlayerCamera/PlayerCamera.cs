8c574d6a31879a659949178ff47cb01086d118b7
Kimdaehun0824 <garamei72@gmail.com>
2023-03-17 15:00:15 +0900

    no message

--- RiskOfRain2/Assets/01. DaehunWork/Scripts/PlayerCamera/PlayerCamera.cs
+++ RiskOfRain2/Assets/01. DaehunWork/Scripts/PlayerCamera/PlayerCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class PlayerCamera : MonoBehaviour
 {
     public float SensitivityX = default;        // 마우스 X축 감도 설정을 위한 값
     public float SensitivityY = default;        // 마우스 Y축 감도 설정을 위한 값
