d4d116452652289d02f1f101ced7cfd345ffe4e6
EricK-00 <carter000@naver.com>
2023-03-24 13:58:56 +0900

    [TaeHee]

--- RiskOfRain2/Assets/TH/Scripts/Runtime/Spit.cs
+++ RiskOfRain2/Assets/TH/Scripts/Runtime/Spit.cs
@@ -27,33 +27,33 @@ public class Spit : MonoBehaviour
 	{
 		if (other.tag == "Ground")
 		{
-			//int pixelSize = 100;
+			int pixelSize = 100;
 
-			//RaycastHit hit;
-			//Vector3 rayVec = Vector3.down;
-			//if (!Physics.Raycast(transform.position, rayVec, out hit))
-			//{
-			//	return;
-			//}
-			//Debug.Log($"[transform] {hit.transform.position}, [texCoord] {hit.textureCoord}");
+			RaycastHit hit;
+			Vector3 rayVec = Vector3.down;
+			if (!Physics.Raycast(transform.position, rayVec, out hit))
+			{
+				return;
+			}
+			Debug.Log($"[transform] {hit.transform.position}, [texCoord] {hit.textureCoord}");
 
-			//Renderer rend = other.GetComponent<MeshRenderer>();
+			Renderer rend = other.GetComponent<MeshRenderer>();
 
-			//Texture2D tex = rend.material.mainTexture as Texture2D;
-			//Vector2 pixelUV = hit.textureCoord;
-			//pixelUV.x *= tex.width;
-			//pixelUV.y *= tex.height;
+			Texture2D tex = rend.material.mainTexture as Texture2D;
+			Vector2 pixelUV = hit.textureCoord;
+			pixelUV.x *= tex.width;
+			pixelUV.y *= tex.height;
 
-			//Color[] colors = new Color[pixelSize * pixelSize];
+			Color[] colors = new Color[pixelSize * pixelSize];
 
-			//for (var i = 0; i < pixelSize * pixelSize; i++)
-			//{
-			//	colors[i] = Color.black;
-			//}
+			for (var i = 0; i < pixelSize * pixelSize; i++)
+			{
+				colors[i] = Color.black;
+			}
 
-			//tex.SetPixels((int)pixelUV.x, (int)pixelUV.y, pixelSize, pixelSize, colors);
+			tex.SetPixels((int)pixelUV.x, (int)pixelUV.y, pixelSize, pixelSize, colors);
 
-			//tex.Apply();
+			tex.Apply();
 
 			Destroy(gameObject);
 		}
