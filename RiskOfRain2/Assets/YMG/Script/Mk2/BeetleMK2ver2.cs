ac72d04099725eb8ae625c4c93ec8cb5e844c4d9
ymg <freedman0829@naver.com>
2023-03-27 17:24:03 +0900

    Dev 0327

--- RiskOfRain2/Assets/YMG/Script/Mk2/BeetleMK2ver2.cs
+++ RiskOfRain2/Assets/YMG/Script/Mk2/BeetleMK2ver2.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,7 +6,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
-public enum MobState { Idle = 0, Run, Attack}
+public enum MobState { Idle = 0, Recognition, Run, Attack}
 
 public class BeetleMK2ver2 : MonoBehaviour
 {
@@ -14,18 +15,22 @@ public class BeetleMK2ver2 : MonoBehaviour
 
 	public float LookRange = 20f; // 시야 영역
 	public float AttackRange = 4f; // 공격 영역
-	public float Speed = default; // 이동속도
+	public float Speed = default; // 속도
 	NavMeshAgent Pathfinder; // 네비매쉬
 	Transform Target; // 목표
 	Animator Anime;
 	public AnimationClip SpawnAnime;
+	public AnimationClip AttackAnime;
+
+	private bool _isLook = default;
+
 
 
 	private void Awake()
 	{
 		Pathfinder = GetComponent<NavMeshAgent>();
 		Anime = GetComponent<Animator>();
-		ChangeState(MobState.Idle);
+		//ChangeState(MobState.Idle);
 	}
 
 	void Start()
@@ -33,34 +38,43 @@ public class BeetleMK2ver2 : MonoBehaviour
 		Target = GameObject.FindGameObjectWithTag("Player").transform;
 
 		Anime.SetBool("isMove", false);
+		Anime.SetBool("isReco", false);
 		Anime.SetBool("isAttack", false);
 		Anime.SetBool("isDead", false);
 
 		StartCoroutine(AnimeWaiting());
+
+		_isLook = false;
+
 	}
 
 	IEnumerator AnimeWaiting()
 	{
-		//Debug.Log($"애니 길이가 몇초? {SpawnAnime.length}");
-		yield return new WaitForSeconds(SpawnAnime.length);
+		//DateTime sourTime = DateTime.Now;
+		////Debug.Log($"애니 길이가 몇초? {SpawnAnime.length}");
+		//yield return new WaitForSeconds(SpawnAnime.length);
+		//DateTime destTime = DateTime.Now;
+		//Debug.Log((destTime - sourTime).Duration());
 
+		yield return new WaitForSeconds(SpawnAnime.length);
 		// 애니메이션 발동 조건 
 		Anime.SetBool("SpawnEnd", true);
 	}
+
     void Update()
     {
 		if (Anime.GetBool("SpawnEnd"))
 		{
 			float distance = Vector3.Distance(Target.position, transform.position);
-
+			//FaceTarget();
 			//if (true) ChangeState(MobState.Idle);
 			if (distance <= LookRange && distance > AttackRange) ChangeState(MobState.Run);
-			if (distance <= AttackRange) ChangeState(MobState.Attack);
+			else if (distance <= AttackRange) ChangeState(MobState.Attack);
 		}
 
 		//UpdateState();
 	}
-	void UpdateState() // LAGACY
+	void UpdateState() // LEGACY
 	{
 		switch (mobState) 
 		{
@@ -92,6 +106,9 @@ public class BeetleMK2ver2 : MonoBehaviour
 
 	void ChangeState(MobState newState) 
 	{
+		if (mobState == newState)
+			return;
+
 		StopCoroutine(mobState.ToString());
 		mobState = newState;
 		StartCoroutine(mobState.ToString());
@@ -100,59 +117,100 @@ public class BeetleMK2ver2 : MonoBehaviour
 	private IEnumerator Idle() // 코루틴 내부에서 루프
 	{
 		// 상태 진입 1회
-			Debug.Log("대기");
+		Pathfinder.SetDestination(transform.position);
+		_isLook = false;
+		Debug.Log("대기");
 		Anime.SetBool("isMove", false);
 		Anime.SetBool("isAttack", false);
 		while (true) // 현재 상태에서 매 프레임
 		{
+
 			yield return null;
 		}
 		// 상태가 종료될 때 1회
 
 	}
-		//yield return null;	//// LAGACY
-		//if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
-		//{
-		//	animator.SetBool("Idle", true);
-		//}
+	//yield return null;	//// LEGACY
+	//if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
+	//{
+	//	animator.SetBool("Idle", true);
+	//}
 
 	private IEnumerator Run() // 코루틴 내부에서 루프
 	{
 		// 상태 진입 1회
-		FaceTarget(); // 적을 바라보고
-		Anime.SetBool("isMove", true);
 		Anime.SetBool("isAttack", false);
-		
+
+		StartCoroutine(Recognition());
+		Anime.SetBool("isMove", true);
+
+		while (true)
+		{
 			Debug.Log("네비");
 			Pathfinder.SetDestination(Target.position); // 적을 향해 간다
-		while (true) // 현재 상태에서 매 프레임
-		{
+
 			yield return null;
 		}
+
 		// 상태가 종료될 때 1회
+		//Pathfinder.SetDestination(transform.position);
 
 	}
+
+
 	private IEnumerator Attack() // 코루틴 내부에서 루프
 	{
 		// 상태 진입 1회
-		FaceTarget(); // 적을 바라보고
+		//FaceTarget(); 
+		Anime.SetBool("isMove", false); // 움직임을 멈춘다
+										//_isLook = true; // 바라본다
+		Debug.Log("움직임을 멈춤.");
+		//StartCoroutine(Recognition());
+		FaceTarget();
+		//ChangeState(MobState.Recognition);
 
-		Pathfinder.SetDestination(transform.position);
-		Anime.SetBool("isMove", false);
-		Anime.SetBool("isAttack", true);
-			Debug.Log("공격");
-		
-		while (true) // 현재 상태에서 매 프레임
-		{
-			yield return null;
-		}
+
+		//StartCoroutine(FaceT()); // 3초 기다리고
+		//_isLook = false; // 안바라본다
+		//StopCoroutine(FaceT());
+		Anime.SetBool("isAttack", true); // 공격한다
+		Anime.SetBool("isReco", false); // 공격한다
+		Debug.Log("공격");
+		yield return null;
+		yield return new WaitForSeconds(1.0f);
+
 		// 상태가 종료될 때 1회
+		Anime.SetBool("isAttack", false);
+		Anime.SetBool("isReco", true);
 
 	}
 
+	private IEnumerator Recognition()
+	{
+		//float distance = Vector3.Distance(Target.position, transform.position);
+
+		//if (distance <= LookRange && distance > AttackRange) ChangeState(MobState.Run);
+		//yield return new WaitForSeconds(3.0f);
+		//if (distance <= AttackRange) ChangeState(MobState.Attack);
+		yield return new WaitForSeconds(3.0f);
+	}
+
+	private IEnumerator Term(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+	}
+
+	//private IEnumerator Term(float delay, System.Action func_) // LEGACY
+	//{
+	//	yield return new WaitForSeconds(delay);
+	//	func_?.Invoke();
+	//}
+
 	void FaceTarget()
 	{
+		//if (_isLook)
 		
+			Debug.Log("바라보기");
 			// direction to the target
 			Vector3 direction = (Target.position - transform.position).normalized;
 			// rotation where we point to that target
@@ -160,6 +218,19 @@ public class BeetleMK2ver2 : MonoBehaviour
 			// update our own rotation to point in this direction
 			transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * Speed);
 		
+	}
+
+	IEnumerator FaceT()
+	{
+		Debug.Log("바라보기");
+		yield return new WaitForSeconds(3.0f);
+		// direction to the target
+		Vector3 direction = (Target.position - transform.position).normalized;
+		// rotation where we point to that target
+		Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+		// update our own rotation to point in this direction
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * Speed);
+
 	}
 
 	void OnDrawGizmosSelected()
