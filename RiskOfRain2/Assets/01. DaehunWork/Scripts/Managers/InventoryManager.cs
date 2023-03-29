7e178b38e6deadd8c9249c054fc5e72d3df3507a
Kimdaehun0824 <garamei72@gmail.com>
2023-03-29 20:40:31 +0900

    [Daehun] -Feat : ItemBase Add -Detail : Item 추가를 위한 Base클래스 추가 해당 클래스를 상속시켜서 상세 아이템 구현은 상속받은 하위 클래스에서 구현할 예정

---
+++ RiskOfRain2/Assets/01. DaehunWork/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,17 @@
+using RiskOfRain2.Item;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+namespace RiskOfRain2.Manager
+{
+	public class InventoryManager : SingletonBase<InventoryManager>
+	{
+		public List<ItemBase> items = new List<ItemBase>();
+
+
+		private new void Awake()
+		{
+			base.Awake();
+		}
+	}
+}
