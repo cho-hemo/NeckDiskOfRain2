af3371317465f6393e0b830b069f1313ea7c451a
Giole13 <gudwns4f12@gmail.com>
2023-04-02 18:33:59 +0900

    [HyungJun] Ending Scene Work end

--- RiskOfRain2/Assets/HyungJun/Scripts/Runtime/Objects/Teleporter.cs
+++ RiskOfRain2/Assets/HyungJun/Scripts/Runtime/Objects/Teleporter.cs
@@ -5,52 +5,52 @@ using UnityEngine;
 
 public class Teleporter : InteractionObjects
 {
-    [SerializeField] private List<GameObject> _bossMonsters;
-    private Transform _bossSpawnSpot;
+	[SerializeField] private List<GameObject> _bossMonsters;
+	private Transform _bossSpawnSpot;
 
-    private void Start()
-    {
-        _bossSpawnSpot = Global.FindRootObject("BossSpawnSpot").transform;
-    }
+	private void Start()
+	{
+		_bossSpawnSpot = Global.FindRootObject("BossSpawnSpot").transform;
+	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag.Equals("Player") && _isActive)
-        {
-            _disposable = true;
-            UIManager.Instance.PopupUIActive(" 텔레포터 가동..?", true);
-        }
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag.Equals("Player") && _isActive)
+		{
+			_disposable = true;
+			UIManager.Instance.PopupUIActive(" 텔레포터 가동..?", true);
+		}
+	}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && _disposable)
-        {
-            Interaction();
-        }
-    }
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E) && _disposable)
+		{
+			Interaction();
+		}
+	}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag.Equals("Player"))
-        {
-            _disposable = false;
-            UIManager.Instance.PopupUIActive("", false);
-        }
-    }
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag.Equals("Player"))
+		{
+			_disposable = false;
+			UIManager.Instance.PopupUIActive("", false);
+		}
+	}
 
-    public override void Interaction()
-    {
-        base.Interaction();
-        _isActive = false;
-        _disposable = false;
-        // 목표 UI오브젝트 세팅을 바꿔주는 함수
-        UIManager.Instance.MissionComplete();
-        UIManager.Instance.PopupUIActive("", false);
+	public override void Interaction()
+	{
+		base.Interaction();
+		_isActive = false;
+		_disposable = false;
+		// 목표 UI오브젝트 세팅을 바꿔주는 함수
+		UIManager.Instance.MissionComplete();
+		UIManager.Instance.PopupUIActive("", false);
 
-        int randomNum = Random.Range(0, _bossMonsters.Count);
-        GameObject boss = Instantiate(_bossMonsters[randomNum], _bossSpawnSpot.position, _bossSpawnSpot.rotation);
-    }
+		int randomNum = Random.Range(0, _bossMonsters.Count);
+		GameObject boss = Instantiate(_bossMonsters[randomNum], _bossSpawnSpot.position, _bossSpawnSpot.rotation);
+	}
 
 
 
